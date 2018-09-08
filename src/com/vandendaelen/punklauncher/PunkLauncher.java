98dad00e90e3d6b387c49545cd9663e98a988dda
LotuxPunk <clement@vandendaelen.com>
2018-09-08 21:55:20 +0200

    first

---
+++ src/com/vandendaelen/punklauncher/PunkLauncher.java
@@ -0,0 +1,12 @@
+package com.vandendaelen.punklauncher;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PunkLauncher extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
