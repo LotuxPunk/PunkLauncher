9a7f6a9d4673d92dd6716b22a078d9ac672ba97f
LotuxPunk <clement@vandendaelen.com>
2018-09-09 18:39:19 +0200

    J'ai commencé ça

--- src/com/vandendaelen/punklauncher/PunkLauncher.java
+++ src/com/vandendaelen/punklauncher/PunkLauncher.java
@@ -1,12 +1,16 @@
 package com.vandendaelen.punklauncher;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class PunkLauncher extends Application {
+
+    private Button btnLaunch;
+    private Button btnLogin;
+    private Button btnLogout;
 
     //Pour montrer à Cala
     public static void main(String[] args) {
@@ -15,9 +19,30 @@ public class PunkLauncher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Punk Launcher");
+
+        btnLaunch = new Button("Lancer");
+        btnLogin = new Button("Se connecter");
+        btnLogout = new Button("Se déconnecter");
+
+        btnLaunch.setOnAction(event ->{
+
+        });
+
+        btnLogin.setOnAction(event -> {
+
+        });
+
+        btnLogout.setOnAction(event -> {
+
+        });
+
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(btnLaunch,btnLogin,btnLogout);
+
+        Scene scene = new Scene(layout,800,500);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
