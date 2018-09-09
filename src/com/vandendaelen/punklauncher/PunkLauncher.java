000ea33968e85897e5e35b13473f4beff748754c
LotuxPunk <clement@vandendaelen.com>
2018-09-09 14:53:52 +0200

    Salut Cala

--- src/com/vandendaelen/punklauncher/PunkLauncher.java
+++ src/com/vandendaelen/punklauncher/PunkLauncher.java
@@ -2,6 +2,7 @@ package com.vandendaelen.punklauncher;
 
 public class PunkLauncher {
 
+    //Pour montrer à Cala
     public static void main(String[] args) {
         Engine engine = new Engine(800,600,"Launcher Imperacube");
     }
