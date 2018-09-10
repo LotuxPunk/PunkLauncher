70f26bb3eb492a1e797f08bdd4116c08ef22c9e6
LotuxPunk <clement@vandendaelen.com>
2018-09-10 14:06:05 +0200

    work on design

--- src/com/vandendaelen/punklauncher/PunkLauncher.java
+++ src/com/vandendaelen/punklauncher/PunkLauncher.java
@@ -1,10 +1,17 @@
 package com.vandendaelen.punklauncher;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class PunkLauncher extends Application {
 
@@ -19,12 +26,42 @@ public class PunkLauncher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Init
+        HBox root = new HBox();
+        HBox loginBox = new HBox();
+        GridPane grid = new GridPane();
+
+        root.setPadding(new Insets(10, 10, 10, 10));
+
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+
         primaryStage.setTitle("Punk Launcher");
 
+        //Buttons grid
         btnLaunch = new Button("Lancer");
+        GridPane.setConstraints(btnLaunch,0,0);
         btnLogin = new Button("Se connecter");
+        GridPane.setConstraints(btnLogin,0,1);
         btnLogout = new Button("Se déconnecter");
+        GridPane.setConstraints(btnLogout,0,2);
 
+        grid.getChildren().addAll(btnLaunch,btnLogin,btnLogout);
+
+        btnLaunch.setPrefSize(200,20);
+        btnLogin.setPrefSize(200,20);
+        btnLogout.setPrefSize(200,20);
+
+        //LoginBox
+        Label labelEmail = new Label("Email:");
+        TextField textEmail = new TextField();
+        Label labelPassword = new Label("Password:");
+        PasswordField textPassword = new PasswordField();
+
+        loginBox.getChildren().addAll(labelEmail, textEmail, labelPassword, textPassword);
+
+        //Events
         btnLaunch.setOnAction(event ->{
 
         });
@@ -37,10 +74,10 @@ public class PunkLauncher extends Application {
 
         });
 
-        StackPane layout = new StackPane();
-        layout.getChildren().addAll(btnLaunch,btnLogin,btnLogout);
+        //Root
+        root.getChildren().addAll(grid, loginBox);
 
-        Scene scene = new Scene(layout,800,500);
+        Scene scene = new Scene(root,800,500);
 
         primaryStage.setScene(scene);
         primaryStage.show();
